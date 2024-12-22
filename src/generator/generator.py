e9dfff91b954dc30ee828ae38b26b7a948a78a64
al3x707g <a.ryb9861@gmail.com>
2024-12-22 22:31:11 +0100

    adjusted fill grid method

--- src/generator/generator.py
+++ src/generator/generator.py
@@ -3,16 +3,17 @@ from src.generator.util.types import GRID
 
 
 class Generator:
+    grid: GRID
 
-    def __init__(self, width: int, height: int):
+    def __init__(self, width: int, height: int) -> None:
         self.width = width
         self.height = height
-        self.grid: GRID = self.get_filled_grid(BlockType.HOOKABLE)
+        self.fill_grid(BlockType.HOOKABLE)
 
-    def get_filled_grid(self, block_type: BlockType) -> GRID:
+    def fill_grid(self, block_type: BlockType) -> GRID:
         grid: GRID = [
             [block_type for _ in range(self.width)]
             for _ in range(self.height)
         ]
 
-        return grid
+        self.grid = grid
