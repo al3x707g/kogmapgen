9622ca37ca27326aee1d01a415d184bd9b15b4a6
al3x707g <a.ryb9861@gmail.com>
2024-12-23 01:39:47 +0100

    convert edge class to dataclass

--- src/generator/graph/edge.py
+++ src/generator/graph/edge.py
@@ -1,41 +1,15 @@
+from dataclasses import dataclass
+
 from vertex import Vertex
 
 
+@dataclass(frozen=True)
 class Edge:
-
-    def __init__(self, v_from: Vertex, v_to: Vertex):
-        self.v_from = v_from
-        self.v_to = v_to
+    v_from: Vertex
+    v_to: Vertex
 
     def has_vertex(self, vertex: Vertex) -> bool:
-        if not vertex:
-            return False
-
         return any(
             v is vertex or (v.x == vertex.x and v.y == vertex.y)
             for v in (self.v_to, self.v_from)
         )
-
-    @property
-    def v_from(self) -> Vertex:
-        return self.v_from
-
-    @v_from.setter
-    def v_from(self, value: Vertex) -> None:
-        self.v_from = value
-
-    @v_from.deleter
-    def v_from(self) -> None:
-        del self.v_from
-
-    @property
-    def v_to(self) -> Vertex:
-        return self.v_to
-
-    @v_to.setter
-    def v_to(self, value: Vertex) -> None:
-        self.v_to = value
-
-    @v_to.deleter
-    def v_to(self) -> None:
-        del self.v_to
