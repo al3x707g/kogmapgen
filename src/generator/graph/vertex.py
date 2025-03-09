9287668934aec35efdfcb2ad0e7b675b8b4fc29c
al3x707g <a.ryb9861@gmail.com>
2025-03-09 19:38:52 +0100

    make vertices hashable

--- src/generator/graph/vertex.py
+++ src/generator/graph/vertex.py
@@ -6,3 +6,11 @@ class Vertex:
     x: int
     y: int
     visited: bool = False
+
+    def __eq__(self, other):
+        if isinstance(other, Vertex):
+            return (self.x, self.y) == (other.x, other.y)
+        return False
+
+    def __hash__(self):
+        return hash((self.x, self.y))
